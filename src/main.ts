52c51beb0666994feabc652a1df7fb747f146a94
gabaissues <gabaissues@gmail.com>
2025-08-20 12:17:20 -0300

    fix: fix port nestjs

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 10000;
   await app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
   });
