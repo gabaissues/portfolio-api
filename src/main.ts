cdf616d4a4e2593e74c205c14e2e6e7b0d332a9a
gabaissues <gabaissues@gmail.com>
2025-08-20 11:57:50 -0300

    fix: init nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-export const app = express();
 
 async function bootstrap() {
-  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(app));
-
-  nestApp.enableCors({
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
     origin: '*',
   });
-  nestApp.useGlobalPipes(new ValidationPipe());
 
-  await nestApp.init();
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(process.env.PORT ?? 8001);
 }
 
 bootstrap();
-
-export default app;
